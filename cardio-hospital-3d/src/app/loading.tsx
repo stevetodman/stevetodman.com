3d6b797f343d3da73e474ec4919c20d91cad0dee
stevetodman <stevetodman@Hotmail.com>
2026-08-14 13:02:56 -0500

    Add Cardio Hospital 3D foundation

---
+++ cardio-hospital-3d/src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <main className="loading-screen">
+      <div className="loading-mark">CH</div>
+      <p>Loading pediatric cardiology...</p>
+    </main>
+  );
+}
