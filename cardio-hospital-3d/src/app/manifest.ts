6b5142dfc029b8693daaa04eff4074c993e9f396
stevetodman <stevetodman@Hotmail.com>
2026-09-03 16:42:30 -0500

    Scope hospital manifest to /hospital

--- cardio-hospital-3d/src/app/manifest.ts
+++ cardio-hospital-3d/src/app/manifest.ts
@@ -5,14 +5,15 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Pediatric Hospital",
     short_name: "Peds Hospital",
     description: "An immersive pediatric clinical reasoning simulation.",
-    start_url: "/",
+    start_url: "/hospital/",
+    scope: "/hospital/",
     display: "standalone",
     orientation: "any",
     background_color: "#0b1215",
     theme_color: "#0b1215",
     icons: [
       {
-        src: "/hospital-icon.svg",
+        src: "/hospital/hospital-icon.svg",
         sizes: "any",
         type: "image/svg+xml",
       },
